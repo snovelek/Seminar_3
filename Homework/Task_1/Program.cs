cdd3baaab1765b7ebb4bc39e9293c0bf9651e0bc
Artem <remenusei@yandex.ru>
2022-07-20 02:29:00 +0700

    fix_1

--- Homework/Task_1/Program.cs
+++ Homework/Task_1/Program.cs
@@ -7,11 +7,10 @@
 Console.Write("Введите пятизначное число - ");
 int n=Convert.ToInt32(Console.ReadLine());
 if (n>9999&&n<100000){
-    if (n%10==n/10000){
-        if ((n%100)/10==(n/1000)%10){
+    if (n%10==n/10000 && (n%100)/10==(n/1000)%10){
             Console.WriteLine("Да");
-        }
     }
+        
     else{
     Console.WriteLine("Нет");
     }
